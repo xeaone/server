9a10859b6ac86e4bb95fa6df9e710c8b956807d9
Alexander Elias <alex.steven.elias@gmail.com>
2022-04-08 13:07:30 -0700

    add deps version

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 
-export * as base64url from 'https://deno.land/std/encoding/base64url.ts';
+export * as base64url from 'https://deno.land/std@0.134.0/encoding/base64url.ts';
 
-export { extname, join } from 'https://deno.land/std/path/mod.ts';
-export { readableStreamFromReader } from 'https://deno.land/std/streams/mod.ts';
-export { Status, STATUS_TEXT, } from 'https://deno.land/std/http/http_status.ts';
+export { extname, join } from 'https://deno.land/std@0.134.0/path/mod.ts';
+export { readableStreamFromReader } from 'https://deno.land/std@0.134.0/streams/mod.ts';
+export { Status, STATUS_TEXT, } from 'https://deno.land/std@0.134.0/http/http_status.ts';
 
-export { serve } from 'https://deno.land/std/http/server.ts';
+export { serve } from 'https://deno.land/std@0.134.0/http/server.ts';
