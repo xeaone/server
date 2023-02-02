8af45cff71ee20659ea908932eaca6c707624eb7
Alexander Elias <alex.steven.elias@gmail.com>
2023-02-02 13:25:45 -0800

    updated deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
-export * as media from 'https://deno.land/std@0.171.0/media_types/mod.ts';
-export * as base64url from 'https://deno.land/std@0.171.0/encoding/base64url.ts';
+export * as media from 'https://deno.land/std@0.176.0/media_types/mod.ts';
+export * as base64url from 'https://deno.land/std@0.176.0/encoding/base64url.ts';
 
-export { extname, join } from 'https://deno.land/std@0.171.0/path/mod.ts';
-export { serve, Status, STATUS_TEXT } from 'https://deno.land/std@0.171.0/http/mod.ts';
-export { readableStreamFromReader } from 'https://deno.land/std@0.171.0/streams/mod.ts';
+export { extname, join } from 'https://deno.land/std@0.176.0/path/mod.ts';
+export { serve, Status, STATUS_TEXT } from 'https://deno.land/std@0.176.0/http/mod.ts';
+export { readableStreamFromReader } from 'https://deno.land/std@0.176.0/streams/mod.ts';
