1978a71b46e055309cc4280e13ef22890054240c
Alexander Elias <alex.steven.elias@gmail.com>
2023-07-26 11:32:39 -0700

    updated deploy script

--- deploy.ts
+++ deploy.ts
@@ -1,22 +1,20 @@
 import { increment, ReleaseType } from 'https://deno.land/std@0.188.0/semver/mod.ts';
 
-const { run, readTextFile, writeTextFile, args } = Deno;
-const [release] = args;
-
+const [release] = Deno.args;
 if (!release) throw new Error('release required');
 
-const f = await run({ cmd: ['git', 'fetch'] }).status();
+const f = await (new Deno.Command('git', { args: ['fetch'] }).spawn()).output();
 if (!f.success) throw new Error('git auth');
 
-const n = await run({ cmd: ['npm', 'whoami'] }).status();
+const n = await (new Deno.Command('npm', { args: ['whoami'] }).spawn()).output();
 if (!n.success) throw new Error('npm auth');
 
-const pkg = JSON.parse(await readTextFile('./package.json'));
+const pkg = JSON.parse(await Deno.readTextFile('./package.json'));
 pkg.version = increment(pkg.version, release as ReleaseType);
 
-await writeTextFile('./package.json', JSON.stringify(pkg, null, '    '));
-await run({ cmd: ['git', 'commit', '-a', '-m', pkg.version] }).status();
-await run({ cmd: ['git', 'push'] }).status();
-await run({ cmd: ['git', 'tag', pkg.version] }).status();
-await run({ cmd: ['git', 'push', '--tag'] }).status();
-await run({ cmd: ['npm', 'publish', '--access', 'public'] }).status();
+await Deno.writeTextFile('./package.json', JSON.stringify(pkg, null, '    '));
+await (new Deno.Command('git', { args: ['commit', '-a', '-m', pkg.version] }).spawn()).output();
+await (new Deno.Command('git', { args: ['push'] }).spawn()).output();
+await (new Deno.Command('git', { args: ['tag', pkg.version] }).spawn()).output();
+await (new Deno.Command('git', { args: ['push', '--tag'] }).spawn()).output();
+await (new Deno.Command('npm', { args: ['publish', '--access', 'public'] }).spawn()).output();
