56ac68e8d5a7fa8c3fb7993918e62f683475660f
Alexander Elias <alex.steven.elias@gmail.com>
2023-10-25 14:35:15 -0700

    deploy fix

--- deploy.ts
+++ deploy.ts
@@ -1,4 +1,4 @@
-import { increment, ReleaseType } from 'https://deno.land/std@0.204.0/semver/mod.ts';
+import { increment, parse, ReleaseType } from 'https://deno.land/std@0.204.0/semver/mod.ts';
 
 const cmd = (cmd: string, args?: string[]) => new Deno.Command(cmd, { args }).spawn().output();
 
@@ -12,11 +12,13 @@ const n = await cmd('npm', ['whoami']);
 if (!n.success) throw new Error('npm auth');
 
 const pkg = JSON.parse(await Deno.readTextFile('./package.json'));
-pkg.version = increment(pkg.version, release as ReleaseType);
+pkg.version = increment(parse(pkg.version), release as ReleaseType);
 
 await Deno.writeTextFile('./package.json', JSON.stringify(pkg, null, '    '));
+
 await cmd('git', ['commit', '-a', '-m', pkg.version]);
 await cmd('git', ['push']);
 await cmd('git', ['tag', pkg.version]);
 await cmd('git', ['push', '--tag']);
+
 await cmd('npm', ['publish', '--access', 'public']);
