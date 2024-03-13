d8e67d7c8d3e14bab980e8ef64f15004008011eb
Alexander Elias <alex.steven.elias@gmail.com>
2024-03-13 11:23:52 -0700

    deps update

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
-export * as media from 'jsr:@std/media-types@0.216';
+export * as media from 'jsr:@std/media-types@0.219.1';
 
-export { ByteSliceStream } from 'jsr:@std/streams@0.216';
-export { STATUS_CODE, STATUS_TEXT } from 'jsr:@std/http@0.216/status';
-export type { StatusCode, StatusText } from 'jsr:@std/http@0.216/status';
+export { ByteSliceStream } from 'jsr:@std/streams@0.219.1';
+export { STATUS_CODE, STATUS_TEXT } from 'jsr:@std/http@0.219.1/status';
+export type { StatusCode, StatusText } from 'jsr:@std/http@0.219.1/status';
 
-export { extname, join } from 'jsr:@std/path@0.216';
-export { calculate, ifNoneMatch } from 'jsr:@std/http@0.216/etag';
-export { decodeBase64Url, encodeBase64Url } from 'jsr:@std/encoding@0.216/base64url';
+export { extname, join } from 'jsr:@std/path@0.219.1';
+export { calculate, ifNoneMatch } from 'jsr:@std/http@0.219.1/etag';
+export { decodeBase64Url, encodeBase64Url } from 'jsr:@std/encoding@0.219.1/base64url';
